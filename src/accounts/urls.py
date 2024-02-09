0a3bbb31fd312d410913c5f0735ea50424fb2245
Sloma1kk <i@sloma1kk.ru>
2024-02-09 18:47:32 +0300

    fourth commit добавлена возможность регистрации пользователя не через админку,

---
+++ src/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from accounts.views import *
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('update/', update_view, name='update'),
+]
+
